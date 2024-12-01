19ba45330dfcae5735ccfe444fd473a8f37b7b7a
brillianintdoh <palanghwi@gmail.com>
2024-12-01 13:19:34 +0900

    삭제

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-source ~/start/.emsdk/emsdk_env.sh
-
-rm /home/wheedo/start/Project7/game/godot/dino_rumble/server/src/main/resources/static/game/*
-find "demo/bin/"* ! -name "*.gdextension" -exec rm {} \; && 
-scons platform=web build_library=no &&
-scons platform=windows build_library=no
-scons platform=linux build_library=no && godot --headless --path ./demo/ -v --export-debug "Web" 
